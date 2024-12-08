f562e80293921638f833048e4ab9683feba4217d
Pîndaru Maria <65163209+MariaPindaru@users.noreply.github.com>
2024-12-08 16:48:40 +0200

    [Backend] Allow cross-origin requests

---
+++ EventPlatform/src/main/java/ro/gt/eventplatform/config/WebConfig.java
@@ -0,0 +1,18 @@
+package ro.gt.eventplatform.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        // Allow cross-origin requests from React
+        registry.addMapping("/api/**")
+                .allowedOrigins("http://localhost:3000")
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedHeaders("*");
+    }
+}
