72cf57f015ed261dbd2fc19bb576561b84a84b2b
Pîndaru Maria <65163209+MariaPindaru@users.noreply.github.com>
2024-12-15 00:33:22 +0200

    [Backend] Small fix

--- EventPlatform/src/main/java/ro/gt/eventplatform/controller/EventController.java
+++ EventPlatform/src/main/java/ro/gt/eventplatform/controller/EventController.java
@@ -14,6 +14,7 @@ import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/events")
+@CrossOrigin(origins = "http://localhost:3000")
 public class EventController {
     private final EventService eventService;
 
